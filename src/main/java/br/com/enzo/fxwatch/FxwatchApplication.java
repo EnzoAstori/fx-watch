a1886cbdcf15d9f39e77239358644dc5be4f3f4a
EnzoAstori <enzorubim10@gmail.com>
2026-07-27 23:57:55 -0300

    feat: inicializa projeto Spring Boot

---
+++ src/main/java/br/com/enzo/fxwatch/FxwatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.enzo.fxwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/enzo/fxwatch/FxwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.enzo.fxwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
