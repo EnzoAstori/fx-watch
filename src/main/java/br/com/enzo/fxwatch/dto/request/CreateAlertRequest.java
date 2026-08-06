0da1e33e55089c0023163dbd3be326b5982125f6
EnzoAstori <enzorubim10@gmail.com>
2026-08-06 20:36:21 -0300

    DTO iniciado

--- src/main/java/br/com/enzo/fxwatch/dto/request/CreateAlertRequest.java
+++ src/main/java/br/com/enzo/fxwatch/dto/request/CreateAlertRequest.java
@@ -1,4 +1,15 @@
 package br.com.enzo.fxwatch.dto.request;
 
+import br.com.enzo.fxwatch.enums.CurrencyCode;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateAlertRequest {
+
+    @NotNull
+    private CurrencyCode currencyCode;
+
 }
--- src/main/java/br/com/enzo/fxwatch/dto/response/AlertResponse.java
+++ src/main/java/br/com/enzo/fxwatch/dto/response/AlertResponse.java
@@ -1,4 +1,30 @@
 package br.com.enzo.fxwatch.dto.response;
 
+import br.com.enzo.fxwatch.enums.CurrencyCode;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class AlertResponse {
+
+    private Long id;
+
+    private CurrencyCode currencyCode;
+
+    private Boolean enabled;
+
+    private BigDecimal lowestPrice;
+
+    private BigDecimal highestPrice;
+
+    private BigDecimal lastPrice;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastCheck;
+
 }
